42396060f90de8f2d37ecf7159dbfdc80aeb49bd
zaidalyafeai <alyafey22@gmail.com>
2022-08-27 18:28:12 +0300

    remove build

--- Tokenization/build/lib/bpe_surgery/const.py
+++
@@ -1,8 +0,0 @@
-SOW = '▁'       # START OF WORD
-MORPH_SEP = '+' # MORPH SEPARATOR
-UNK = '<unk>'   # UNKNOWN
-PAD = '<pad>'   # PADDING
-SEG_SEP = "<seg>"
-SOS = '<s>'
-EOS = '</s>'
-RESERVERD_WORDS = [PAD, UNK, SOS, EOS] 
