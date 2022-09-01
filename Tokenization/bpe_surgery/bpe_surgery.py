3ec93f83e1530063ad4d9839ddab0237e1e82aaf
zaidalyafeai <alyafey22@gmail.com>
2022-09-01 09:48:36 +0300

    fix bug in encode_sentences in segmentation algorithm

--- Tokenization/bpe_surgery/bpe_surgery.py
+++ Tokenization/bpe_surgery/bpe_surgery.py
@@ -444,9 +444,11 @@ class bpe:
 
     output = []
     if self.seg:
-      joined_sentences = ' <sep> '.join(sentences)
+      
+      joined_sentences = ' #sep# '.join(sentences)
       seg_joined_setnences = self.segmenter.segment(joined_sentences)
-      sentences = [sentence.strip() for sentence in seg_joined_setnences.split('<sep>')]
+      assert len(sentences) == len(seg_joined_setnences.split('#sep#'))
+      sentences = [sentence.strip() for sentence in seg_joined_setnences.split('#sep#')]
 
     pbar = tqdm(total=len(sentences)) 
     for stmt in sentences:
